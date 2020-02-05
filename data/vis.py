4254023895fbef1994be9860814766134a26f47b
Alkis Gotovos <alkisg@mit.edu>
2020-02-05 00:42:46 -0500

    More automation (include yaml options file)

--- data/vis.py
+++ data/vis.py
@@ -1,18 +1,17 @@
+import os
 import numpy as np
 import matplotlib.pylab as plt
 import pickle as pcl
 import csv
+import yaml
 
 
-fname = 'HeFlatMiddle2Sign'
 #               t     i     s      col   width
 nodeformat = '{0:d} {1:d} {2:.5f} {3:d} {4:.1f}\n'
 #              t1    i1    t2    i2   col1  col2  width1  width2
 edgeformat = '{0:d} {1:d} {2:d} {3:d} {4:d} {5:d} {6:.1f} {7:.1f}\n'
-slo = -2.5
-shi = 2.5
-WMIN = 0
-IMAX = 4
+
+FNAME = 'FlatInitRightSign40'
 
 
 def relu(x):
@@ -38,56 +37,81 @@ def xy2str(x, y):
 
 
 if __name__ == '__main__':
-    with open(fname + '.pcl', 'rb') as fin:
+    # Remove all existing .txt files
+    allfiles = os.listdir(FNAME)
+    for file in allfiles:
+        if file.endswith('.txt'):
+            os.remove(os.path.join(FNAME, file))
+    # Read and write options
+    with open(FNAME + '/options.yml') as ymlstream:
+        try:
+            options = yaml.safe_load(ymlstream)
+        except yamlYAMLError as exception:
+            print(exception)
+    ILIST = options['ILIST']
+    TLIST = options['TLIST']
+    WMIN = options['WMIN']
+    SLO = options['SLO']
+    SHI = options['SHI']
+    XLO = options['XLO']
+    XHI = options['XHI']
+    YLO = options['YLO']
+    YHI = options['YHI']
+    with open(FNAME + '/options.txt', 'w') as fout:
+        writer = csv.writer(fout, delimiter=' ')
+        writer.writerow([str(p) for p in [SLO, SHI, XLO, XHI, YLO, YHI]])
+    # Read data
+    with open(FNAME + '/' + FNAME + '.pcl', 'rb') as fin:
         aouts, souts, louts, gouts, x, y = pcl.load(fin)
     print(len(aouts))
     whi = np.max([np.max(np.abs(aouts[i][1:-1])) for i in range(1, len(aouts))])
     wlo = 0
     nodestring = ''
     edgestring = ''
-    with open('../' + fname + '_data.txt', 'w') as fout:
+    with open(FNAME + '/data.txt', 'w') as fout:
         xy = sorted(list(zip(x[0], y[0])), key=lambda x: x[0])
         writer = csv.writer(fout, delimiter=',')
         writer.writerows(xy)
     nodeset = set()
-    for i in range(1, IMAX):
-        #fx = np.linspace(slo, shi, 1000)
-        fx = souts[i]
-        fy = flatnet(fx, aouts[i], souts[i])
-        with open('../' + fname + '_xy_' + str(i) + '.txt', 'w') as fout:
+    for i, si in enumerate(ILIST):
+        fxbefore = np.linspace(-5, souts[si][0], 10)
+        fxafter = np.linspace(souts[si][-1], 5, 10)
+        fx = np.hstack((fxbefore, souts[si], fxafter))
+        fy = flatnet(fx, aouts[si], souts[si])
+        with open(FNAME + '/xy_' + str(i) + '.txt', 'w') as fout:
             writer = csv.writer(fout, delimiter=',')
             writer.writerows(zip(fx, fy))
-        for j, s in enumerate(souts[i]):
-            if s < slo:
-                s = slo
-            elif s > shi:
-                s = shi
-            st = trans(s, slo, shi)
-            w = aouts[i][j+1]
+        for j, s in enumerate(souts[si]):
+            if s < SLO:
+                s = SLO
+            elif s > SHI:
+                s = SHI
+            st = trans(s, SLO, SHI)
+            w = aouts[si][j+1]
             if w > 0:
                 col = 8
             else:
                 col = 7
             wt = trans(np.fabs(w), wlo, whi)
             if wt > WMIN:
-                nodeset.add((i-1, j))
-                nodestring += nodeformat.format(i-1, j, st, col, wt)
-            if i > 1:
-                curlabels = [int(x) for x in louts[i][1]]
-                prevlabels = [int(x) for x in louts[i-1][1]]
+                nodeset.add((i, j))
+                nodestring += nodeformat.format(i, j, st, col, wt)
+            if i > 0:
+                curlabels = [int(x) for x in louts[si][1]]
+                prevlabels = [int(x) for x in louts[si-1][1]]
                 thislabel = curlabels[j]
                 try:
                     prevj = prevlabels.index(thislabel)
-                    if (i-1, j) in nodeset and (i-2, prevj) in nodeset:
-                        edgestring += edgeformat.format(i-2, prevj, i-1, j, col, 8, 0.5, 0.5)
+                    if (i, j) in nodeset and (i-1, prevj) in nodeset:
+                        edgestring += edgeformat.format(i-1, prevj, i, j, col, 8, 0.5, 0.5)
                 except ValueError:
                     pass
-    with open('../' + fname + '_nodes.txt', 'w') as nout:
+    with open(FNAME + '/nodes.txt', 'w') as nout:
         nout.write(nodestring)
-    with open('../' + fname + '_edges.txt', 'w') as nout:
+    with open(FNAME + '/edges.txt', 'w') as nout:
         nout.write(edgestring)
-    with open('../' + fname + '_times.txt', 'w') as nout:
+    with open(FNAME + '/times.txt', 'w') as nout:
         timestring = ''
-        for i in range(1, IMAX):
-            timestring += '1000\n'
+        for t in TLIST:
+            timestring += '{0:d}\n'.format(t)
         nout.write(timestring)
